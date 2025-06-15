120c88e3b9ddaafe5ddb569b93e2e644f8055fd7
Prasad Rangapu <164445957+Prasad-rangapu@users.noreply.github.com>
2025-06-15 19:03:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,16 @@
-const mysql = require('mysql2');
+
 require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
-}).promise();
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
-module.exports = connection;
+module.exports = pool;
